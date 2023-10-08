d6479de92f8c6eb220082433a987a6efe4fc7807
Nischall01 <thapanischal39@gmail.com>
2023-10-08 18:28:18 +0545

    Updates

--- DDMS.c
+++ DDMS.c
@@ -36,14 +36,15 @@ int main();
 void menu();
 void submenu_userdata();
 int getMenuChoice();
-int wanttocontinue();
 void clearInputBuffer();
 void hold();
 void notriesleft();
 void clrs();
 void UI();
 void dtime(int *, int *, int *, int *, int *, int *);
+void login();
 void sign_up();
+int checkforspaces(char *);
 int addrecord();
 int openrecord();
 int editrecord();
@@ -61,7 +62,6 @@ void checkuserfile();
 bool exit_module(const char *);
 
 // Universal Variables
-int tries = 5;
 int isnotint;
 char buffer[MAX_LINE];
 char name[CH_LIMIT];  // This variable holds the name
@@ -69,91 +69,120 @@ char pass[CH_LIMIT];  // This variable holds the password
 char email[CH_LIMIT]; // This variable holds the email address
 char line[MAX_LINE];
 
-// Main Function
 int main()
 {
     clrs();
     checkuserfile();
-    int choice, choice2;
+    login();
+    UI();
+    return 0;
+}
+
+void login()
+{
+    int passwordtries = 5;
+    int emailtries = 5;
     char enteredpass[CH_LIMIT];
     char enteredeaddy[CH_LIMIT];
-    // Extract the username, password and email address.
+    // Extract the username, password and email address from user info file.
     FILE *file;
     file = fopen("Program_files/user_info.dat", "r");
     fscanf(file, "%s %s %s", name, pass, email);
     fclose(file); // File handling STOPs.
 
     //  Login Page
-    printf("\t\tWelcome, %s\n", name);
-    while (1)
+    int i;
+    int namelength = strlen(name);
+    printf("\t\t-------------");
+    for (i = 0; i < namelength; i++)
     {
-        printf("\n\tEnter the password => ");
+        printf("-");
+    }
+    printf("\n");
+    printf("\t\t| Welcome, %s |", name);
+    printf("\n");
+    printf("\t\t-------------");
+    for (i = 0; i < namelength; i++)
+    {
+        printf("-");
+    }
+    do
+    {
+        printf("\n");
+        printf("\n\tEnter password => ");
         scanf("%s", enteredpass);
         if (strcmp(enteredpass, pass) == 0)
         {
+            break;
+        }
+        else if (strcmp(enteredpass, "EXIT") == 0)
+        {
             clrs();
-            UI();
+            printf("\tHave a great Day.");
+            nap(1.7);
+            exit(0);
         }
         else if (strcmp(enteredpass, "RESET") == 0)
         {
             clrs();
-            int trys = 5;
-        try_email:
-            printf("\nEnter your email address entered during the login or enter 'EXIT' to exit:\n=> ");
-            scanf("%s", enteredeaddy);
-            if (strcmp(enteredeaddy, email) == 0)
+            do
             {
-                clrs();
-                editpassword();
-                main();
-            }
-            else if (strcmp(enteredeaddy, "EXIT") == 0)
-            {
-                main();
-            }
-            else
-            {
-                clrs();
-                trys--;
-                printf("\nIncorrect email address. Please try again. Tries left: %d\n", trys);
-                if (trys == 0) // if wrong password is entered more than '5' times the program will terminate.
+                printf("\nEnter your email address entered during the login or enter 'EXIT' to exit:\n=> ");
+                scanf("%s", enteredeaddy);
+                if (strcmp(enteredeaddy, email) == 0)
                 {
-                    notriesleft();
+                    clrs();
+                    editpassword();
+                    main();
                 }
-                goto try_email;
-                break;
+                else if (strcmp(enteredeaddy, "EXIT") == 0)
+                {
+                    main();
+                }
+                else
+                {
+                    clrs();
+                    emailtries--;
+                    printf("\nIncorrect Email address. Please try again. Tries left: %d\n", emailtries);
+                    if (emailtries == 0) // if wrong email is entered more than '5' times the program will terminate.
+                    {
+                        notriesleft();
+                    }
+                }
+            } while (emailtries > 0);
+            if (emailtries == 0)
+            {
+                notriesleft();
             }
         }
         else
         {
-            tries--;
+            passwordtries--;
             clrs();
-            printf("\nIncorrect, please try again or if you forgot your password enter 'RESET'. Tries left: %d", tries);
-            if (tries == 0) // if wrong password is entered more than '5' times the program will terminate.
-            {
-                notriesleft();
-            }
-            continue;
+            printf("\nIncorrect Password, please try again or if you forgot your password enter 'RESET'. Enter 'EXIT' to exit. Tries left: %d", passwordtries);
         }
+    } while (passwordtries > 0);
+
+    if (passwordtries == 0) // if wrong password is entered more than '5' times the program will terminate.
+    {
+        notriesleft();
     }
-    return 0;
 }
 
 void UI()
 {
-    int choice;
+    clrs();
     while (1)
     {
-        int choice;
         clrs();
         reminders();
         menu();
-        choice = getMenuChoice();
-
-        switch (choice)
+        int choice1;
+        choice1 = getMenuChoice();
+        clrs();
+        switch (choice1)
         {
         case 1:
-            clrs();
             if (addrecord() == 0)
             {
                 break;
@@ -223,26 +252,27 @@ void UI()
             break;
 
         case 5:
-            clrs();
             while (1)
             {
                 clrs();
-                int choice2;
+                char choice2;
                 submenu_userdata();
-                choice2 = getMenuChoice();
+                printf(" => ");
+                scanf("%c", &choice2);
                 switch (choice2)
                 {
-                case 1:
+                case '1':
                     editusername();
                     continue;
-                case 2:
+                case '2':
                     editpassword();
                     continue;
-                case 3:
+                case '3':
                     editemailaddress();
                     continue;
-                case 4:
-                    clrs();
+                case '4':
+                    break;
+                case '.':
                     break;
                 default:
                     clrs();
@@ -256,20 +286,19 @@ void UI()
             break;
 
         case 6:
-            clrs();
             while (1)
             {
                 clrs();
-                int choice3;
+                char choice3;
                 submenu_reminders();
-                choice3 = getMenuChoice();
+                printf(" => ");
+                scanf("%c", &choice3);
                 switch (choice3)
                 {
-                case 1:
-                    clrs();
+                case '1':
                     addreminder();
                     continue;
-                case 2:
+                case '2':
                     clrs();
                     if (reminders() == 0)
                     {
@@ -279,7 +308,7 @@ void UI()
                     }
                     deletereminder();
                     continue;
-                case 3:
+                case '3':
                     clrs();
                     if (reminders() == 0)
                     {
@@ -288,8 +317,9 @@ void UI()
                     }
                     clearreminders();
                     continue;
-                case 4:
-                    clrs();
+                case '4':
+                    break;
+                case '.':
                     break;
                 default:
                     clrs();
@@ -332,10 +362,17 @@ void UI()
 }
 
 // Function Definitions
+
+void clrs()
+{
+    system("cls");
+    printf("\033[H\033[J");
+}
+
 void checkuserfile()
 {
     FILE *file;
-    file = fopen("Program_files/user_info.dat", "r+"); // Opens the file the holds user info to read
+    file = fopen("Program_files/user_info.dat", "r"); // Opens the file the holds user info to read
     bool isEmptyLine = true;
     int lineCount = 1;
     char ch;
@@ -362,9 +399,6 @@ void checkuserfile()
         if (file_size == 0 || file == 0) // Checks if the file exists or not & if it does, checks if it's empty or not.
         {
             fclose(file);
-            printf("Error in the user file. Please Signup\n");
-            printf("\nPress any key to continue !\n");
-            getchar();
             sign_up();
         }
         else if (lineCount < 3)
@@ -384,51 +418,53 @@ void checkuserfile()
     }
     fclose(file);
 }
-void clrs()
-{
-    system("cls");
-    printf("\033[H\033[J");
-}
+
 void clearInputBuffer()
 {
     while (getchar() != '\n')
         ;
 }
+
 void press()
 {
     printf("\nPress any key to continue !\n");
     getchar();
     clrs();
 }
+
 void menu()
 {
-    printf("1. Add a new record\n");
-    printf("2. Open a record\n");
-    printf("3. Edit a record\n");
-    printf("4. Delete a record\n");
-    printf("5. Edit User data\n");
-    printf("6. Reminders\n");
-    printf("7. Help/About\n");
-    printf("8. Exit\n");
+    printf("______________________\n");
+    printf("(1) Add a new record\n");
+    printf("(2) Open a record\n");
+    printf("(3) Edit a record\n");
+    printf("(4) Delete a record\n");
+    printf("(5) Edit User data\n");
+    printf("(6) Reminders\n");
+    printf("(7) Help/About\n");
+    printf("(8) Exit\n");
 }
+
 void submenu_userdata()
 {
-    printf("1. Edit Username\n");
-    printf("2. Edit Password\n");
-    printf("3. Edit Email Address\n");
-    printf("4. Exit\n");
+    printf("(1) Edit Username\n");
+    printf("(2) Edit Password\n");
+    printf("(3) Edit Email Address\n");
+    printf("(4) Exit\n");
 }
+
 void submenu_reminders()
 {
-    printf("1. Add reminder\n");
-    printf("2. Remove reminder\n");
-    printf("3. Clear reminders\n");
-    printf("4. Exit\n");
+    printf("(1) Add reminder\n");
+    printf("(2) Remove reminder\n");
+    printf("(3) Clear reminders\n");
+    printf("(4) Exit\n");
 }
+
 int getMenuChoice()
 {
     int choice;
-    printf("=> ");
+    printf(" => ");
     if (scanf("%d", &choice) != 1)
     {
         printf("Invalid input. Please enter a number.\n");
@@ -443,32 +479,7 @@ int getMenuChoice()
         return choice;
     }
 }
-int wanttocontinue()
-{
-    while (1)
-    {
-        clrs();
-        char yn[2];
-        printf("Enter 'y' to continue and 'n' to exit.\n=> ");
-        scanf("%1s", yn);
-        clearInputBuffer();
-        if (strcmp(yn, "y") == 0)
-        {
-            return 0;
-        }
-        else if (strcmp(yn, "n") == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            clrs();
-            printf("\tInvalid input. Please enter a valid option.");
-            getchar();
-            continue;
-        }
-    }
-}
+
 void notriesleft()
 {
     clrs();
@@ -525,8 +536,7 @@ int reminders()
         fclose(file);
         reminders();
     }
-    printf("Reminders:\n");
-    printf("\n");
+    printf("\tReminders\n       ```````````\n");
     fseek(file, 0, SEEK_END);     // Move the file pointer to the end of the file
     long file_size = ftell(file); // Get the position of the file pointer (i.e., the file size)
     fclose(file);
@@ -549,6 +559,7 @@ int reminders()
         return 1;
     }
 }
+
 void sign_up()
 {
     clrs();
@@ -651,23 +662,51 @@ void sign_up()
     fclose(file);
     printf("Sign up successful!\n");
     press();
-    // system("PDMS.exe");
+    main();
 }
 
 int addrecord()
 {
+    clrs();
     FILE *file;
     dtime(&dt.year, &dt.month, &dt.day, &dt.hour, &dt.min, &dt.sec);
     char record_name[50];
+    char lc_record_name[50];
+    int record_name_len;
     printf("Record name: ");
-    scanf("%s", record_name);
+    scanf(" %[^\n]", record_name);
     if (exit_module(record_name))
     {
         return 0;
     }
+    if (checkforspaces(record_name) == 1)
+    {
+        clrs();
+        printf("Invalid input! A record name shouldn't contain any spaces.");
+        getchar();
+        clrs();
+        return 0;
+    }
+
+    // Remove newline character from the input
+    size_t len = strlen(record_name);
+    if (len > 0 && record_name[len - 1] == '\n')
+    {
+        record_name[len - 1] = '\0';
+    }
+    // Initialize lc_record_name to all null characters
+    memset(lc_record_name, '\0', sizeof(lc_record_name));
+    // Convert each character to lowercase and store in lc_record_name
+    for (int i = 0; record_name[i]; i++)
+    {
+        lc_record_name[i] = tolower((unsigned char)record_name[i]);
+    }
+
+    // Add the record in the record entry
     file = fopen("Program_files/Records.dat", "a");
-    fprintf(file, "(%d-%02d-%02d) %s \n", dt.year, dt.month, dt.day, record_name);
+    fprintf(file, "(%d-%02d-%02d) %s\n", dt.year, dt.month, dt.day, lc_record_name);
     fclose(file);
+
     strcat(record_name, ".txt");
     printf("\n%s\n", record_name);
     file = fopen(record_name, "w");
@@ -681,12 +720,12 @@ int addrecord()
         // Enter the record data
         char record_data[100];
         printf("=> ");
-        scanf(" %[^\n]", record_data); // Read input until newline is encountered
+        scanf(" %[^\n]", record_data);
         if (exit_module(record_data))
         {
-            return 1;
+            return 0;
         }
-        // Add the new record to the record file
+        // Add the record text to the new record file
         fprintf(file, "\n%s", record_data);
         fclose(file);
         printf("\nNew record added successfully.");
@@ -837,20 +876,46 @@ int deleterecord()
 
 int addreminder()
 {
+    clrs();
     char when[2];
     FILE *file;
     dtime(&dt.year, &dt.month, &dt.day, &dt.hour, &dt.min, &dt.sec);
     char reminder_title[50], date[15];
-    int day, hour, minute;
+    int month, day, hour, minute;
     int intid;
+    int oldintid;
     char stringid[5];
     printf("Reminder Id: ");
     scanf("%d", &intid);
     sprintf(stringid, "%d", intid);
+    // Check if the id is longer than 3 letters.
+    if (strlen(stringid) > 3)
+    {
+        clrs();
+        printf("Invalid input! Ids shouldn't be longer than 3 letters.");
+        hold();
+        return 0;
+    }
+    // Check if the ID already exists
+    FILE *fp;
+    fp = fopen("Program_files/Reminders.dat", "r");
+    // Read and check the first 5 characters of each line
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        if (strncmp(stringid, line, strlen(stringid)) == 0)
+        {
+            clrs();
+            printf("A reminder with same Id already exits. Enter a different Id.");
+            hold();
+            return 0;
+        }
+    }
+
     if (exit_module(stringid))
     {
         return 0;
     }
+
     printf("Reminder Title: ");
     scanf(" %[^\n]", reminder_title);
     if (exit_module(reminder_title))
@@ -874,20 +939,35 @@ int addreminder()
         strcpy(date, "Today");
         break;
     case '2':
-        strcpy(date, "Tommorrow");
+        strcpy(date, "Daily");
         break;
     case '3':
         printf("Enter Day: ");
-        scanf("%d", day);
+        scanf("%d", &day);
         if (day > 30)
         {
-            printf("Please enter a valid day.");
+            clrs();
+            printf("Please, enter a valid month.\n");
+            hold();
+            clrs();
+            clearInputBuffer();
             addreminder();
         }
         sprintf(date, "%d/%d", day, dt.month);
         break;
-    case '.':
-        return 0;
+    case '4':
+        printf("Enter Month: ");
+        scanf("%d", &month);
+        if (month > 12)
+        {
+            clrs();
+            printf("Please, enter a valid month.\n");
+            hold();
+            clrs();
+            clearInputBuffer();
+            addreminder();
+        }
+        break;
     default:
         break;
     }
@@ -1050,6 +1130,7 @@ void editusername()
     printf("\tUsername replaced successfully.\n");
     hold();
 }
+
 void editpassword()
 {
     char replacement[50];
@@ -1162,6 +1243,7 @@ bool exit_module(const char *filename)
     }
     return false; // File doesn't exist
 }
+
 void hold()
 {
     clearInputBuffer();
@@ -1178,4 +1260,17 @@ void dtime(int *y, int *m, int *d, int *h, int *mi, int *s)
     *h = t.wHour;
     *mi = t.wMinute;
     *s = t.wSecond;
+}
+
+int checkforspaces(char *record_name)
+{
+    int i;
+    size_t len = strlen(record_name);
+    for (i = 0; i < len; i++)
+    {
+        if (record_name[i] == ' ')
+        {
+            return 1;
+        }
+    }
 }
